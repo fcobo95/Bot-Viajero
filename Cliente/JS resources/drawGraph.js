271811fa72f619ac76687431c6a622de5f9c7c43
Erick Fernando Cobo <fcobo95@gmail.com>
2017-03-23 10:49:01 -0600

    Comments and other additions. -Erick Cobo

---
+++ Cliente/JS resources/drawGraph.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Erick Fernando Cobo on 3/23/2017.
+ */
+
+$(document).ready(function () {
+    $(".graph").on('click', function () {
+        alert("You clicked the button!")
+    })
+});
