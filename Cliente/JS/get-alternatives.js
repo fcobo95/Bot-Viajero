b6cb63979565a7a11d642c13cd5d85e4e65d666e
Joshua <jcamposc889@ulacit.ed.cr>
2017-04-08 20:50:16 -0600

    Funcionalidad en botón de Get Alternatives y muestra de resultado.

--- Cliente/JS/get-alternatives.js
+++ Cliente/JS/get-alternatives.js
@@ -1,9 +1,9 @@
 function alternativas() {
     $("#get-alternatives").click(function (event) {
+        event.preventDefault();
         var origen = $("#Origen").val();
         var destino = $("#Destino").val();
         var prioridad = $("#Prioridad").val();
-        event.preventDefault();
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -16,58 +16,80 @@ function alternativas() {
             "processData": false,
             "data": JSON.stringify({Origen: origen, Destino: destino, Prioridad: prioridad}),
             success: function (response) {
-                for (var alternativa = 0; alternativa < 4; alternativa++) {
-                    var index = 1;
-                    var ruta = response['Orden'];
+                var html = ""
+                $.each(response, function (llave, valor) {
+                    var ruta = response[llave]['Orden'];
                     var viajes = ruta.length - 1;
-                    var html = "<div class='container-fluid col-md-3' id='alternativa'><h4>Ruta: " + ruta + "</h4>";
-                    if (ruta[i['Alternativa' + index]]) {
-                        for (var i = 0; i < viajes; i++) {
-                            var transporte = response[ruta[i]];
-                            var nodo = ruta[i];
-                            html += "<div id='nodo'><label>" + (i + 1) + ") " + nodo + "</label><br>";
-                            if (transporte.hasOwnProperty('Bus')) {
-                                html += "<label>Transporte: Bus</label><br><ul>";
-                                for (var key in transporte['Bus']) {
-                                    if (key != 'Ruta') {
-                                        html += "<li>" + key + " : " + transporte['Bus'][key] + "</li>";
-                                    }
+                    html = "<div class='container-fluid col-md-8' id='ruta'><h4>Ruta: " + ruta + "</h4>";
+                    for (var i = 0; i < viajes; i++) {
+                        var transporte = response[llave][ruta[i]];
+                        var nodo = ruta[i];
+                        html += "<div id='nodo'><label>" + (i + 1) + ") " + nodo + "</label><br>";
+                        if (transporte.hasOwnProperty('Bus')) {
+                            html += "<label>Transporte: Bus</label><br><ul>";
+                            for (var key in transporte['Bus']) {
+                                if (key !== 'Ruta') {
+                                    html += "<li>" + key + " : " + transporte['Bus'][key] + "</li>";
                                 }
-                                html += "</ul></div></div>"
-                            } else if (transporte.hasOwnProperty('Taxi')) {
-                                html += "<label>Transporte: Taxi</label><ul>";
-                                for (var key in transporte['Taxi']) {
-                                    if (key != 'Ruta') {
-                                        html += "<li>" + key + " : " + transporte['Taxi'][key] + "</li>";
-                                    }
-                                }
-                                html += "</ul></div></div>"
-                            } else if (transporte.hasOwnProperty('Tren')) {
-                                html += "<label>Transporte: Tren</label><br><ul>";
-                                for (var key in transporte['Tren']) {
-                                    if (key != 'Ruta') {
-                                        html += "<li>" + key + " : " + transporte['Tren'][key] + "</li>";
-                                    }
-                                }
-                                html += "</ul></div></div>"
-                            } else {
-                                html += "<label>Transporte: Avion</label><br><ul>";
-                                for (var key in transporte['Avion']) {
-                                    if (key != 'Ruta') {
-                                        html += "<li>" + key + " : " + transporte['Avion'][key] + "</li>";
-                                    }
-                                }
-                                html += "</ul></div></div>"
                             }
+                            html += "</ul></div></div>"
+                        } else if (transporte.hasOwnProperty('Taxi')) {
+                            html += "<label>Transporte: Taxi</label><ul>";
+                            for (var key in transporte['Taxi']) {
+                                if (key !== 'Ruta') {
+                                    html += "<li>" + key + " : " + transporte['Taxi'][key] + "</li>";
+                                }
+
+                                if (key === 'Conductor') {
+                                    html += "<ul>";
+                                    for (var value in transporte['Taxi']['Conductor']) {
+                                        html += "<li>" + value + " : " + transporte['Taxi']['Conductor'][value] + "</li>";
+                                    }
+                                    html += "</ul>";
+                                }
+
+                            }
+                            html += "</ul></div></div>";
+
+                        } else if (transporte.hasOwnProperty('Tren')) {
+                            html += "<label>Transporte: Tren</label><br><ul>";
+                            for (var key in transporte['Tren']) {
+                                if (key !== 'Ruta') {
+                                    html += "<li>" + key + " : " + transporte['Tren'][key] + "</li>";
+                                }
+
+                                if (key === 'Horario') {
+                                    html += "<ul>";
+                                    for (var value in transporte['Tren']['Horario']) {
+                                        html += "<li>" + value + " : " + transporte['Tren']['Horario'][value] + "</li>";
+                                    }
+                                    html += "</ul>";
+                                }
+                            }
+                            html += "</ul></div></div>"
+                        } else {
+                            html += "<label>Transporte: Avion</label><br><ul>";
+                            for (var key in transporte['Avion']) {
+                                if (key !== 'Ruta') {
+                                    html += "<li>" + key + " : " + transporte['Avion'][key] + "</li>";
+                                }
+
+                                if (key === 'Horario') {
+                                    html += "<ul>";
+                                    for (var value in transporte['Avion']['Horario']) {
+                                        html += "<li>" + value + " : " + transporte['Avion']['Horario'][value] + "</li>";
+                                    }
+                                    html += "</ul>";
+                                }
+                            }
+                            html += "</ul></div></div>"
                         }
                     }
-                    index++;
-                }
+                })
                 var alternativas = $("#rutas-alternativas");
                 alternativas.html(html);
                 alternativas.css({"color": "red", "opacity": "0.3"});
                 alternativas.animate({"color": "white", "opacity": "1", "fontSize": "2.2em", "padding": "100px"}, 2000);
-                console.log(response);
             },
             error: function (response) {
                 console.log(response);
