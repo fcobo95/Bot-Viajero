24cae2858adc29c8e1b9c1e2d5a57fc5199a0ee9
Erick Fernando Cobo <fcobo95@gmail.com>
2017-04-08 23:28:33 -0600

    Se arregla toda la parte del frontend de get-route.js. Ya la informacion de puede visualizar de manera legible y sin problema alguno. (Y)

--- Cliente/JS/get-routes.js
+++ Cliente/JS/get-routes.js
@@ -18,19 +18,32 @@ function rutas() {
         success: function (response) {
             var ruta = response['Orden'];
             var viajes = ruta.length - 1;
-            var html = "<div class='container-fluid col-md-8' id='ruta'><h4>Ruta: " + ruta + "</h4>";
+            var html = "<div class='container-fluid' id='ruta'>";
+            html += "<div id='ruta-nodo'";
+            html += "<h2>Ruta: " + ruta + "</h2>";
+            html += "</div>";
             for (var i = 0; i < viajes; i++) {
                 var transporte = response[ruta[i]];
                 var nodo = ruta[i];
-                html += "<div id='nodo'><label>" + (i + 1) + ") " + nodo + "</label><br>";
+                html += "<div id='nodo'  class='row'>";
+                html += "<div id='titulo-nodo'>";
+                html += "<label>" + (i + 1) + ') ' + nodo + "</label>";
+                html += "</div>";
                 if (transporte.hasOwnProperty('Bus')) {
-                    html += "<label>Transporte: Bus</label><br><ul>";
+                    html += "<div id='transporte-nodo' class='col'>";
+                    html += "<label>Transporte: Bus</label>";
+                    html += "</div>";
+                    html += "<div id='lista-nodo' class='col'>";
+                    html += "<ul>";
                     for (var key in transporte['Bus']) {
                         if (key !== 'Ruta') {
                             html += "<li>" + key + " : " + transporte['Bus'][key] + "</li>";
                         }
                     }
-                    html += "</ul></div></div>"
+                    html += "</ul>";
+                    html += "</div>";
+                    html += "</div>";
+                    html += "</div>";
                 } else if (transporte.hasOwnProperty('Taxi')) {
                     html += "<label>Transporte: Taxi</label><ul>";
                     for (var key in transporte['Taxi']) {
@@ -43,11 +56,13 @@ function rutas() {
                             for (var value in transporte['Taxi']['Conductor']) {
                                 html += "<li>" + value + " : " + transporte['Taxi']['Conductor'][value] + "</li>";
                             }
-                            html += "</ul>";
                         }
 
                     }
-                    html += "</ul></div></div>";
+                    html += "</ul>";
+                    html += "</div>";
+                    html += "</div>";
+                    html += "</div>";
 
                 } else if (transporte.hasOwnProperty('Tren')) {
                     html += "<label>Transporte: Tren</label><br><ul>";
@@ -61,10 +76,12 @@ function rutas() {
                             for (var value in transporte['Tren']['Horario']) {
                                 html += "<li>" + value + " : " + transporte['Tren']['Horario'][value] + "</li>";
                             }
-                            html += "</ul>";
                         }
                     }
-                    html += "</ul></div></div>"
+                    html += "</ul>";
+                    html += "</div>";
+                    html += "</div>";
+                    html += "</div>";
                 } else {
                     html += "<label>Transporte: Avion</label><br><ul>";
                     for (var key in transporte['Avion']) {
@@ -80,7 +97,10 @@ function rutas() {
                             html += "</ul>";
                         }
                     }
-                    html += "</ul></div></div>"
+                    html += "</ul>";
+                    html += "</div>";
+                    html += "</div>";
+                    html += "</div>";
                 }
             }
             var rutas = $("#detalles-ruta");
@@ -100,6 +120,6 @@ function rutas() {
 }
 
 function logout() {
-    localStorage.removeItem("Token")
+    localStorage.removeItem("Token");
     window.location.href = 'login.html'
 }
