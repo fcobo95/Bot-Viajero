f5f14863efb236d113e0d2ff01a74dd8315fafb0
Erick Fernando Cobo Enriquez <fcob95@hotmail.com>
2017-04-09 19:59:50 -0600

    Documentacion del cliente, tambien, se refactorizan funciones de login y signup a sus archivos .js independientes.

--- Cliente/JS/get-routes.js
+++ Cliente/JS/get-routes.js
@@ -1,11 +1,36 @@
 function rutas() {
+    /* *******************************************************************
+     *
+     * HACEMOS TARGET A LOS BOTONES DEL HTML DE ROUTES, PARA ASI
+     * PODER BRINDARLES CIERTAS BONDADES, APLICANDO EN ALGUNOS CASOS
+     * CLASES COMO BTN Y BTN-PRIMARY O .BUTTON() O .SELECT(), EN EL CASO
+     * DE LOS DROPDOWN MENUS DE ORIGEN Y DESTINO. TAMBIEN, ESTAMOS DECLARANDO
+     * CIERTOS ATRIBUTOS QUE VAN A SER UTILIZADOS PARA ARMAR LOS SETTINGS DEL
+     * CLIENTE PARA PODER HACER LOS PEDIDOS AJAX Y CORS.
+     *
+     * ****************************************************************** */
     var origen = $("#Origen").val();
     var destino = $("#Destino").val();
     var prioridad = $("#Prioridad").val();
-    event.preventDefault();
     var ip = "192.168.1.27";
     var local = "127.0.0.1";
     var port = "5000";
+    event.preventDefault();
+
+    /* ******************************************************************
+     *
+     * AQUI CREAMOS LOS HEADERS QUE VAMOS A ENVIAR POR MEDIO DE AJAX
+     * TAMBIEN, SETEAMOS CIERTOS ATRIBUTOS PARA PODER HACER PEDIDOS
+     * ASINCRONOS, CROSSDOMAIN, Y PARA QUE EL SERVIDOR NOS DEVUELVA
+     * UN POST CON LOS DATOS SOLICITADOS A LA RUTA QUE SE ESPECIFICA
+     * EN EL URL. TAMBIEN, CABE MENCIONAR QUE LO QUE VA A RECIBIR EL
+     * CLIENTE ES UNA RESPUESTA JSON, LOS CUALES SON FORMATEADOS CON
+     * LA LOGICA QUE SE ENCUENTRA EN ESTE ARCHIVO.
+     *
+     * TAMBIEN, TENEMOS LA AUTENTICACION POR MEDIO DE TOKEN, LA CUAL
+     * SE GUARDA EN EL LOCAL STORAGE DEL NAVEGADOR WEB.
+     *
+     * ****************************************************************** */
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -18,8 +43,18 @@ function rutas() {
         "processData": false,
         "data": JSON.stringify({Origen: origen, Destino: destino, Prioridad: prioridad}),
         success: function (response) {
+            /* ******************************************************************
+             *
+             * AQUI, CREAMOS LA ESTRUCTURA QUE VA A CONTENER LOS DATOS QUE VIENEN EN EL JSON
+             * DE LA RESPUESTA DEL LLAMADO AJAX.
+             *
+             * SE CREA TODA LA ESTRUCUTRA DEL HTML. POR MEDIO DE CICLOS ACCEDEMOS LA INFORMACION
+             * QUE VIENE EN EL JSON Y LA IMPRIMIMOS EN SUS RESPECTIVOS LUGARES. SE VERIFICA CADA
+             * LLAVE Y EL VALOR QUE TIENE LA LLAVE, PARA ASI PODER IMPRIMIR LA LLAVE ADECUADA CON
+             * SU VALOR CORRESPONDIENTE.
+             *
+             * ****************************************************************** */
             var index = 0;
-
             var ruta = response['Orden'];
             var viajes = ruta.length - 1;
             var html = "<div class='container-fluid' id='contenedor'>";
@@ -120,6 +155,16 @@ function rutas() {
                     html += "</div>";
                 }
             }
+
+            /* ******************************************************************
+             *
+             * AQUI, FORMATEAMOS POR MEDIO DE TARGETS EL CSS DE LOS ELEMENTOS QUE SE CREAN
+             * EN LA FUNCION DE SUCCESS. ESTOS ELEMENTOS NO SE CREAN DIRECTAMENTE EN EL HTML
+             * DE RUTAS, SINO QUE SE CREAN POR MEDIO DE REFERENCIAS Y MEMORIA. ESTA ESTRUCTURA
+             * UNICAMENTE VIVE CUANDO SE CREA UN LLAMADO AJAX, YA QUE FISICAMENTE, NO SE ENCUENTRA
+             * EN EL CODIGO HTML.
+             *
+             * ***************************************************************** */
             var rutas = $("#detalles-ruta");
             rutas.html(html);
             rutas.css({"color": "red", "opacity": "0.3"});
@@ -137,6 +182,12 @@ function rutas() {
             alert("Error: " + response['Mensaje'])
         }
     };
+
+    /* ******************************************************************
+     *
+     * SE CARGAN LOS SETTINGS ANTERIORES Y SE ENVIA EL REQUEST AJAX.
+     *
+     * ***************************************************************** */
 
     $.ajax(settings).done(function (response) {
         console.log(response)
